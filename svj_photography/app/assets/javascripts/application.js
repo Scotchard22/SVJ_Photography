4a80281303a6eea34da1f3cb55e3f018da758116
Scotchard22 <scotchard@aol.com>
2015-11-12 20:12:11 -0500

    fixed footer to bottom

--- svj_photography/app/assets/javascripts/application.js
+++ svj_photography/app/assets/javascripts/application.js
@@ -19,9 +19,28 @@
       $('.parallax').parallax();
     });
 
-var bumpIt = function() {
-        $('body').css('margin-bottom', $('#footer').height());
+$(document).ready(function(){
+  $(".button-collapse").sideNav({edge: 'left'});
+​
+  var bumpIt = function() {
+        $('body').css('margin-bottom', $('.footer').height());
       },
       didResize = false;
 ​
   bumpIt();
+​
+  $(window).resize(function() {
+    didResize = true;
+  });
+  setInterval(function() {
+    if(didResize) {
+      didResize = false;
+      bumpIt();
+    }
+  }, 250);
+​
+  $('button').click(function() {
+    $(this).parent().append('<p>Another paragraph</p>');
+    bumpIt();
+  });
+});

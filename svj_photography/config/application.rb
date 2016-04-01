1c1ae6660f5d33cf8cbad7ab5a132fd482c137f4
Scotchard22 <scotchard@aol.com>
2016-04-01 01:28:37 -0400

    ready to host

--- svj_photography/config/application.rb
+++ svj_photography/config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+HOSTNAME = ENV['HOSTNAME'] || 'localhost'
+
 module SvjPhotography
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
